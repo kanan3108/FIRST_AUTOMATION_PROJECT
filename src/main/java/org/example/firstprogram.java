bc647b5fddf1edc8d343826248a506279f7cd365
KANAN <nawaniankit69@gmail.com>
2025-08-12 15:58:09 +0530

    compatibility change

--- src/main/java/org/example/firstprogram.java
+++ src/main/java/org/example/firstprogram.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 import java.time.Duration;
 import java.util.List;
@@ -13,6 +15,7 @@ import java.util.List;
 public class firstprogram {
     public static void main(String[] args) throws InterruptedException {
 
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
         driver.manage().window().maximize();
